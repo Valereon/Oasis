d376b0d6f86bc9f0bf93b98aa3331541a0126aa9
Valereon <tools418@gmail.com>
2023-09-13 21:02:17 -0400

    new loading screen and working sounds/flicker
    Co-authored-by: Spencer A Warren <Spencer-Warren@users.noreply.github.com>

---
+++ docs/game/loadingScreen.js
@@ -0,0 +1,20 @@
+
+
+export function loadingScreen() {
+    // let percentComplete = 0;
+    // var progressBar = $("#progress-bar")
+    
+    // while (percentComplete < 100) {
+    //     wait(1000);
+    //     percentComplete += 10;
+    //     console.log(percentComplete)
+    //     progressBar.css("width", percentComplete + "%");
+    // }
+
+}
+
+// function wait(timeout) {
+//     return new Promise(resolve => {
+//         setTimeout(resolve, timeout);
+//     });
+// }
--- docs/game/main.js
+++ docs/game/main.js
@@ -1,90 +1,9 @@
-import {dialouge, setup} from "./dialouge.js";
+import { loadingScreen } from "./loadingScreen.js"
 
-let buttons = {
-    id : {
-        buttonId : "button1",
-        text : "Hello?",
-        style : "outlined-button",
-        func : function() {
-            incrementCounter("counter1", 1);
-        }
-    }    
-};
-
-
-let counters = {
-    id : {
-        counterId : "counter1",
-        text : "0",
-        style : "counter"
-    }
-}
-
-
-
-function setupButtons(){
-    Object.values(buttons).forEach(element => {
-
-
-        document.body.appendChild(div);
-    });
-}
-
-
-
-
-
-
-function setupCounters(){
-    Object.values(counters).forEach(element => {
-        let counter = document.createElement("div");
-        counter.id = element.counterId;
-        counter.innerHTML = element.text;
-        counter.classList.add(element.style);
-
-        let div = document.createElement("div");
-        div.appendChild(counter);
-        div.classList.add("center-vert-horiz-above");
-
-        document.body.appendChild(div);
-    });
-}
-
-
-
-
-
-
-function incrementCounter(counterId, strength){
-    let counter = document.getElementById(counterId);
-    counter.innerHTML = parseInt(counter.innerHTML) + 1 * strength;
-}
-
-
-
-
-function autoIncrementCounter(counterId, strength, interval){
-    setInterval(function(){
-        incrementCounter(counterId, strength);
-    }, interval);
-}
 
 
 
 window.onload = function() {
-    let title = document.createElement("h1");
-    title.innerHTML = "Prologue";
-    title.classList.add("center-vert-horiz-above");
-    title.classList.add("titleLarge");
-    document.body.appendChild(title);
-
-
-    new Promise(resolve => setTimeout(resolve, 7000)).then(() => {
-       // setupButtons();
-        document.body.removeChild(title);
-        setup();
-        dialouge();
-    });
-
-
+    let startupSound = new Audio("./assets/sound/startup.wav");
+    startupSound.play();
 }
