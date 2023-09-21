fe74ad4678c46b0bd603376001eccb1a462587f2
Spencer A Warren <spencer.warren.19@cnu.edu>
2023-09-21 18:45:32 -0400

    Map loading complete

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -4,6 +4,9 @@ window.onload = function () {
     sessionStorage.setItem("currentPage", "powerButton");
     // TODO: Delete this 
     changePage("home");
+    setTimeout(function () {
+        loadASCIIMap("TestColor");
+    }, 1000);
     // let powerButton = document.getElementById("play-power-button");
     // powerButton.addEventListener("click", function () {
     //     changePage("boot", startBoot);
@@ -29,3 +32,52 @@ export function changePage(pageName, callBack=null) {
             }
         );
 }
+
+export function loadASCIIMap(mapName) {
+    let path = "./maps/" + mapName + ".csv";
+    fetch(path)
+    .then( responese => responese.text() )
+    .then( data => loadMap(data) );
+}
+
+export function loadMap(data) {
+    let rawData = data.split("\n");
+    let lastLine = rawData[rawData.length - 1].split(",");
+    let width = parseInt(lastLine[0]);
+    let height = parseInt(lastLine[1]);
+
+
+    let mapData = new Array(height + 1);
+    for (let i = 0; i <= height; i++) {
+        mapData[i] = new Array(width);
+    }
+
+    for (let i = 1; i < rawData.length; i++) {
+        let lineData = rawData[i].split(",");
+        let x = parseInt(lineData[0]);
+        let y = parseInt(lineData[1]);
+        let char = String.fromCharCode(lineData[2]);
+        let color = lineData[3];
+        let bgColor = lineData[4];
+        mapData[x][y] = {ASCII: char, TileColor: color, BGColor: bgColor};
+    }
+
+    mapData.reverse();
+    for (let i = 0; i < mapData.length; i++) {
+        mapData[i].reverse();
+    }
+
+    let pre = document.createElement("pre");
+    for (let i = 0; i < mapData.length; i++) {
+        for (let j = 0; j < mapData[i].length; j++) {
+            let tile = document.createElement("span");
+            tile.innerText = mapData[i][j].ASCII + "";
+            tile.style.color = mapData[i][j].TileColor;
+            tile.style.backgroundColor = mapData[i][j].BGColor;
+            pre.appendChild(tile);
+        }
+        pre.appendChild(document.createElement("br"));
+    }
+    
+    document.getElementById("map").appendChild(pre);
+}    
